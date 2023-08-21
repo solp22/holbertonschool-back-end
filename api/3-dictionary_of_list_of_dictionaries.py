cddd0640633830d7b4a4678fec52861282e530ea
solp22 <sol.puentereal@gmail.com>
2023-08-21 09:30:54 -0700

    fix

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -5,23 +5,23 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    usr_id = 1
     to_do = requests.get(
-        f"https://jsonplaceholder.typicode.com/todos?userId={usr_id}")
+        f"https://jsonplaceholder.typicode.com/todos")
     user = requests.get(
         f"https://jsonplaceholder.typicode.com/users/")
 
     with open("todo_all_employees.json", 'w', encoding="utf-8") as f:
-        task_list = []
         user_dict = {}
 
         for employee in user.json():
+            task_list = []
             username = employee.get('username')
             for task in to_do.json():
-                comp = task.get('completed')
-                tit = task.get('title')
-                task_list.append(dict(task=tit, completed=comp, username=username))
-            user_dict[usr_id] = task_list
-            usr_id = usr_id + 1
+                if employee.get('id') == task.get('userId'):
+                    comp = task.get('completed')
+                    tit = task.get('title')
+                    task_list.append(
+                        dict(task=tit, completed=comp, username=username))
+            user_dict[employee.get('id')] = task_list
 
         json.dump(user_dict, f)
