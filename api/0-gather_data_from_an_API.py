80078c032b194741ff499d881f86ae37d56b8c0b
solp22 <sol.puentereal@gmail.com>
2023-08-21 07:00:56 -0700

    fix url and sytntax errors

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,16 +6,16 @@ from sys import argv
 
 if __name__ == "__main__":
     to_do = requests.get(
-        f"https://jsonplaceholder.typicode.com/todos/todos?userId={argv[1]}")
+        f"https://jsonplaceholder.typicode.com/todos?userId={argv[1]}")
     user = requests.get(
-        f"https://jsonplaceholder.typicode.com/todos/users/{argv[1]}")
+        f"https://jsonplaceholder.typicode.com/users/{argv[1]}")
     employee_name = user.json().get('name')
     done_tasks = 0
     total_tasks = 0
     task_titles = []
 
-    for task in to_do:
-        if to_do.json().get('comlpeted') == 'True':
+    for task in to_do.json():
+        if task.get('completed') is True:
             done_tasks = done_tasks + 1
             total_tasks = total_tasks + 1
             task_titles.append(task.get('title'))
@@ -25,4 +25,4 @@ if __name__ == "__main__":
     print(f"Employee {employee_name} is done with ", end="")
     print(f"tasks({done_tasks}/{total_tasks}):")
     for task in task_titles:
-        print(f"/t  {task}")
+        print(f"\t {task}")
