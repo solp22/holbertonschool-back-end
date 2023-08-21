c2ea76955ff0333b03e3d4181ae7921dbf510bdb
solp22 <sol.puentereal@gmail.com>
2023-08-21 07:33:54 -0700

    1. Export to CSV

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""exporting data to csv"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    to_do = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={argv[1]}")
+    user = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{argv[1]}")
+    username = user.json().get('username')
+
+    with open(f"{argv[1]}.csv", 'w', encoding="utf-8") as f:
+        for task in to_do.json():
+            comp = task.get('comlpeted')
+            tit = task.get("""title""")
+            f.write(
+                f'"{argv[1]}","{username}","{comp}","{tit}" \n')
